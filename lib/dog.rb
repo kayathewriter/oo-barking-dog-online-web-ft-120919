546d44e57cf9460b0fb40d9c7954c3b77d874b2a
Mikael Kaya <kayaswork@gmail.com>
2020-01-05 23:45:09 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,16 @@
 class Dog 
   this dog has spots
+end  
+  def name=(Steve)
+    this_dogs_name = Steve
+  end
+  def name
+    this_dogs_name
+  end
+end  def name=(Steve)
+    this_dogs_name = Steve
+  end
+  def name
+    this_dogs_name
+  end
 end
